ae886f188db6b59a9d616b62a67ae45bef2fc293
Andrey Komanetsky <zeppeli.g@yandex.ru>
2022-01-16 12:15:32 +0300

    v 1.0.10

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get 'signup'  => 'users#new'
   get 'users' => 'welcome#users'
   get 'tranport' => 'welcome#tranport'
+  resources :users
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
